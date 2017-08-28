0141b3a950fbdf604f5047e74b36cc0300f6fe7e
Anish <anish@doctual.com>
2017-08-29 02:33:02 +0300

    app initialize, search bar container

---
+++ src/containers/SearchBar.js
@@ -0,0 +1,35 @@
+import React , {Component} from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            term: ''
+        };
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({term : event.target.value});        
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input 
+                    placeholder="Get weather forecast"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange.bind(this)}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">submit</button>
+                </span>
+            </form>
+        )
+    }
+}
